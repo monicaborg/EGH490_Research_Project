db860b83a285d7825b522f1aba4693ff8b06dbbb
Monica Borg <107089640+monicaborg@users.noreply.github.com>
2026-04-15 21:18:56 +1000

    Add init

---
+++ egh490/__init__.py
@@ -0,0 +1,7 @@
+"""EGH490 Research Project: Explainable Automated Scoring of Conceptual
+Reasoning in Signals & Systems.
+
+See README.md for project scope, supervisors, and ethics statement.
+"""
+
+__version__ = "0.1.0"
