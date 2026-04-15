eed46e1afea73d6b057e6d443c2d17b74ece1437
Monica Borg <107089640+monicaborg@users.noreply.github.com>
2026-04-15 21:23:13 +1000

    Add init

--- egh490/__init__.py
+++ egh490/__init__.py
@@ -1,7 +1,23 @@
-"""EGH490 Research Project: Explainable Automated Scoring of Conceptual
-Reasoning in Signals & Systems.
+"""Cross-cutting utilities: seeding, logging, I/O, config loading.
 
-See README.md for project scope, supervisors, and ethics statement.
+Re-exports the public API so callers can write::
+
+    from egh490.utils import set_global_seed, get_logger, load_config
+
+instead of importing each helper from its individual module.
 """
 
-__version__ = "0.1.0"
+from egh490.utils.config import load_config
+from egh490.utils.io import load_json, load_yaml, save_json, save_yaml
+from egh490.utils.logging import get_logger
+from egh490.utils.seeding import set_global_seed
+
+__all__ = [
+    "get_logger",
+    "load_config",
+    "load_json",
+    "load_yaml",
+    "save_json",
+    "save_yaml",
+    "set_global_seed",
+]
